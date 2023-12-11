63bf048cac51936fb742eeafda7fb5166945af5c
TayanRakimov <kg.tayan@gmail.com>
2023-12-11 22:58:43 +0600

    09-12

---
+++ 09-12/index.js
@@ -0,0 +1,56 @@
+// function display(){
+//     for(let i = 0; i <arguments.length; i++){
+
+    
+//     console.log(arguments[i]);}
+
+// }
+
+// display('Good');
+
+// function number(a=15, b=23){
+  
+//     if(a<b) return a.number;
+     
+// }
+
+// console.log(return);
+
+// function number1(37)
+
+
+
+// Home Work
+
+
+
+
+/*let arr = [1, 12, 37, 4, 77, 8, 5, 10];
+for (let i = 0; i < arr.length; i++) {
+    if(arr[i] > 0 && arr[i] < 10) {
+     console.log(arr[i]);
+    }
+}*/
+
+
+// function display(x){
+// console.log(x);
+// const z = x * 3;
+// console.log(z);
+    
+// }
+
+// display(5);
+
+
+
+function getname() {
+    var name= "Василий";
+    var name;
+    alert("Привет, " + name);
+    if(name === underfined)
+    {
+        alert=("Привет, гость");
+    }
+}
+getname();
