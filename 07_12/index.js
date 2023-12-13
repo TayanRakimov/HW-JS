ef4680f3cbb3baa412e2f07904e47f0685c6cbfc
TayanRakimov <kg.tayan@gmail.com>
2023-12-13 23:34:16 +0600

    HW

--- 07_12/index.js
+++ 07_12/index.js
@@ -1,18 +1,24 @@
-/*var num = 1;
-while(num<30){
-  num++;
-  if(num%2) console.log(num);
+/*var i = 1;
+while(i<=30) {
+  console.log(i);
+  i+=2;
 }*/
 
-/*var num = 30;
-while(num<=2){
-    num++;
-    if(num%1) console.log(num);
-}*/
 
-var arr = [1, 2, 115, 4, 58, 5 ,7 ,99];
-for (var i = 4; i < arr.length; i++)
-if (arr[i] == 4) {
-alert('Есть!');
-break;
-}
+// var i = 30;
+// while(i>=2) {
+//   console.log(i);
+//   i-=2;
+// }
+
+
+// var arr = [1, 2, 115, 4, 58, 5 ,7 ,99];
+// for (var i = 0; i < arr.length; i++) {
+// if (arr[i] == 4) 
+// console.log('Есть!');
+
+// }
+
+
+// var x = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+// for (var y = 0; )
