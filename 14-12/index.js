6b63bd03860e60e44872028f7a430b0986542ac9
TayanRakimov <kg.tayan@gmail.com>
2023-12-14 23:10:50 +0600

    'hw'

--- 14-12/index.js
+++ 14-12/index.js
@@ -1,7 +1,21 @@
-const user = [
-    {name: 'Rakimov', age:85},
-    {name: 'Tayan', age:29},
-    {name: 'Dosuevich', age: 60}
-]
-const info = user.map(item =>  item.name );
-console.log(info); 
+// const user = [
+//     {name: 'Rakimov', age:85},
+//     {name: 'Tayan', age:29},
+//     {name: 'Dosuevich', age: 60}
+// ]
+// const info = user.map(item =>  item.name );
+// console.log(info); 
+
+
+const finalball = (exam, projects) => {
+    if (exam > 90 && projects >10) { return 99;}
+    else if (exam > 75 && projects > 5 ) { return 90;}
+    else if (exam > 50 && projects > 2 ) { return 75;}
+    else if (exam > 30 && projects > 1 ) { return 50;}
+    else { return 0;}
+  }
+  console.log(finalball(99, 11));
+  console.log(finalball(85, 8));
+  console.log(finalball(49, 4));
+  
+ 
