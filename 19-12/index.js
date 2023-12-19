05ef692aace6e94f6f4eb0fa1c4e8f017f1e44e3
TayanRakimov <kg.tayan@gmail.com>
2023-12-19 19:15:23 +0600

    CW

---
+++ 19-12/index.js
@@ -0,0 +1,11 @@
+const user = [
+    {name: 'Rakimov', age:85},
+    {name: 'Tayan', age:29},
+    {name: 'Dosuevich', age: 60}
+]
+const info = user.map(item =>  item.age < 18 );
+console.log(info); 
+
+
+
+ 
