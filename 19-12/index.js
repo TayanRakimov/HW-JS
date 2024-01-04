aa6c0a180383ffde75e45d011869b578f12a1001
TayanRakimov <kg.tayan@gmail.com>
2024-01-04 18:01:19 +0600

    'hw'

--- 19-12/index.js
+++ 19-12/index.js
@@ -1,11 +1,26 @@
-const user = [
-    {name: 'Rakimov', age:85},
-    {name: 'Tayan', age:29},
-    {name: 'Dosuevich', age: 60}
-]
-const info = user.map(item =>  item.age < 18 );
-console.log(info); 
+// const user = [
+//     {name: 'Rakimov', age:85},
+//     {name: 'Tayan', age:29},
+//     {name: 'Dosuevich', age: 60}
+// ]
+// const info = user.map(item =>  item.age < 18 );
+// console.log(info); 
 
 
 
- 
+//  const result =  myArray.reduce(func, initialValue);
+
+
+// const prices = [100, 95, 50];
+// const result = prices.reduce((value, item) => sum + item, 0);
+
+// console.log(result);
+
+
+
+// const prices = [100, 95, 50];
+// let sum = 0;
+// prices.forEach(price => sum += price);
+
+// console.log(sum);
+
---
+++ 21-12/index.js
@@ -0,0 +1,13 @@
+// const date = [1,2,3];
+// const result = myfunc(date);
+// let i = 2;
+
+
+// let count = 0;
+// const intervalid = setInterval(() => {console.log('Hello World');
+// count += 1;
+// if (count === 5) {
+//     alert('done');
+//     clearInterval(intervalid);
+// }
+// },1000);
