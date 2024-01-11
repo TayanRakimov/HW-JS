52450718f0f081d9fb9ff202be5fb83591b95b0d
TayanRakimov <kg.tayan@gmail.com>
2024-01-11 23:15:27 +0600

    New

---
+++ 04-01/script.js
@@ -0,0 +1,31 @@
+
+function myFunc(elementId, text) {
+    document.getElementById(elementId).innerText = newfun;
+  }
+ 
+  newfun=('desc', 'hello world');
+  console.log(newfun);
+
+
+
+  const btn = document.getElementById('continue');
+
+// Onmouseenter
+
+btn.onmouseenter = () => {
+        alert('hello');
+    }
+
+
+// onmouseleave
+
+// btn.onmouseleave = () => {
+//     alert('hello');
+// }
+
+
+// onmousemove
+
+// btn.onmousemove = () => {
+//     alert('hello');
+// }
---
+++ 06-01/script.js
@@ -0,0 +1,5 @@
+const btn = document.getElementById('click');
+
+btn.addEventListener('onclick', () => {
+    alert('Wellcome');
+} );
